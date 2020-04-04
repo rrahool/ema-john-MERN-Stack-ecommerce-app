5f1ea79ead6e97ae09d7fb6d6cc2fc2783bf68e7
rrahool <rbiswas596@gmail.com>
2020-04-05 02:54:13 +0600

    Dynamic Shipment Form, Form Validation, and Styling thr Form and Error Message

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useAuth } from '../Login/useAuth';
+import './Shipment.css'
 
 const Shipment = () => {
+    const { register, handleSubmit, errors } = useForm()
+    const onSubmit = data => { console.log(data) }
+  
+    const auth = useAuth();
+  
     return (
-        <div>
-            <h1>This is Shipment</h1>
-        </div>
-    );
+      <form className="shipment-form" onSubmit={handleSubmit(onSubmit)}>
+        <input name="name" defaultValue={auth.user.name} placeholder="Name" ref={register({ required: true })} />
+        {errors.name && <span className="error">Name is required</span>}
+
+        <input name="email" defaultValue={auth.user.email} placeholder="Email" ref={register({ required: true })} />
+        {errors.email && <span className="error">Email is required</span>}
+
+        <input name="addressLine1" placeholder="Address Line 1" ref={register({ required: true })} />
+        {errors.addressLine1 && <span className="error">Address is required</span>}
+
+        <input name="addressLine2" placeholder="Address Line 2" ref={register} />
+
+        <input name="city" placeholder="City"  ref={register({ required: true })} />
+        {errors.city && <span className="error">City is required</span>}
+
+        <input name="country" placeholder="Country" ref={register({ required: true })} />
+        {errors.country && <span className="error">Country is required</span>}
+
+        <input name="zipcode" placeholder="Zip Code"  ref={register({ required: true })} />
+        {errors.zipcode && <span className="error">Zip Code is required</span>}
+        
+        <input type="submit" />
+      </form>
+    )
 };
 
 export default Shipment;
