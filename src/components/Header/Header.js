4e09ce6cb5543e66a3f6412526d58e03445ec45f
rrahool <rbiswas596@gmail.com>
2020-04-04 23:11:55 +0600

    usePrevious hook - plus, minus button and functionalities added for Counting Previous Value

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../images/logo.png'
 import './Header.css'
 import { useContext } from 'react';
 import { UserContext } from '../../App';
+import { useRef } from 'react';
+import { useEffect } from 'react';
+
+const usePrevious = value => {
+    const prev = useRef();
+    useEffect(() => {
+        console.log(value);
+        
+        prev.current = value;
+    })
+
+    return prev.current;
+}
 
 const Header = () => {
     const user = useContext(UserContext);
+    const [count, setCount] = useState(0);
+    const previous = usePrevious(count);
     return (
         <div className="header">
             <img src={logo} alt=""/>
+            <h1>Count: {count} | Previous: {previous}</h1>
+            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <br/>
             <nav>
                 <a href="/shop">Shop</a>
                 <a href="/review">Order Review</a>
